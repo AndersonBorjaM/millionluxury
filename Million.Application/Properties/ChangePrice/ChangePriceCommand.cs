1745a13d55effde607f4dabd8b2908bf1d548b0c
AndersonBorja <anderson.borja57@gmail.com>
2025-02-10 20:50:24 -0500

    Cambiar precio propiedad

---
+++ Million.Application/Properties/ChangePrice/ChangePriceCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Million.Application.Abstractions.Messaging;
+
+namespace Million.Application.Properties.ChangePrice
+{
+    public sealed record ChangePriceCommand(decimal NewPrice, int IdProperty): ICommand<string>;
+}
